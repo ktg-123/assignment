1350d2e3d78ce130591ca7475e17d6654f0a4bc4
Kunal Khandelwal <khandelwal.kunal4@gmail.com>
2023-01-02 23:42:51 +0530

    Add video model

--- finvid/models.py
+++ finvid/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    publish_datetime = models.DateTimeField()
+    thumbnail = models.URLField()
+    video_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-publish_datetime']
